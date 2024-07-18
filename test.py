eb3a8e0304ef5227b2b55a6443bb05a82e1c4c00
CJ <1736411067@qq.com>
2024-07-18 16:06:50 +0800

    循环

---
+++ test.py
@@ -0,0 +1,15 @@
+'''
+多行注释
+'''
+# 单行注释
+
+#条件判断
+money  = int(input("输入你的钱："))
+if money > 600:
+    print("enough")
+elif money > 400:
+    print("a little")
+else:
+    print("not enough")
+
+#循环语句
