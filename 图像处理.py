32e97c1e79dcd12ce9fde542c4f0e13ebab035dd
cjfinghting <1736411067@qq.com>
2024-07-28 19:00:04 +0800

    赋值，赋值，视图 20240728 上午 直方图 散点图 箱型图 轮廓图 饼图 文字注释箭头 图像处理

---
+++ 图像处理.py
@@ -0,0 +1,15 @@
+# pillow模块处理图片
+# from PIL import Image
+# import numpy as np
+# import matplotlib.pyplot as plt
+# ganyu = Image.open('C:/Users/cj/OneDrive/图片/屏幕快照/2022-04-14 (2).png')
+# ganyu_data = np.array(ganyu)
+# # 红绿蓝 -> 绿红蓝
+# ganyu_data = ganyu_data[:,:,[1, 0, 2]]
+#
+# print(ganyu_data)
+# plt.imshow(ganyu_data)
+# plt.axis('off')
+# plt.show()
+
+# matplotlib包中的图像模块
